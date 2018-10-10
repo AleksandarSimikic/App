f509464a8cce79143a6dc572938f3f6ac1d7c94e
Aleksandar Simikic <sakac_ultra@live.com>
2018-10-10 14:01:04 +0200

    Added inital server and client side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('this is server...');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//server is running on port 
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// get rout
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'MESSAGE' });
+});
